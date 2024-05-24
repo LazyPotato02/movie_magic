0e467f6920a0cad572d2a46c8fdd15ac755c4e70
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-05-24 17:03:02 +0300

    Added rendering on home,details,404,create,about

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,17 @@
 const {Router} = require('express')
+const {home, details, search} = require("../controllers/catalog");
+const {about} = require("../controllers/about");
+const {createGet} = require("../controllers/movie");
+const {notFound} = require("../controllers/404");
 
 const router = Router()
 
+router.get('/',home);
+router.get('/about',about);
+router.get('/create',createGet)
+router.get('/details/:id',details);
+router.get('/search',search)
+router.get('*',notFound)
 // Add routes
 
 module.exports = {
